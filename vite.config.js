9f334c94991cbf8035834c004a65ce44b0c9cd91
Utsava Raj <utsavaraj123@gmail.com>
2026-09-21 21:35:20 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: '/Utsava_Raj_Portfolio/',
+
   server: {
     host: '0.0.0.0',
     port: 3000,
